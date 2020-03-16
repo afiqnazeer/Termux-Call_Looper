81ef478c5b50ec9886d62369bb06ad4d99566c58
khan saad <32749921+khansaad1275@users.noreply.github.com>
2020-03-16 23:58:23 +0530

    Update Call_Looper.sh

--- Call_Looper.sh
+++ Call_Looper.sh
@@ -1,32 +1,38 @@
 
 echo "  ▄▄██████▄▄   | Call-Looper " | lolcat -s 100000
 echo "▄██▀▄█▄▄█▄▀██▄ | Version 1.0" | lolcat -s 100000
-echo "▀▀▀▄██▀▀██▄▀▀▀ | Github/khansaad1275" | lolcat -s 100000
-echo " ▄███─██─███▄  | " | lolcat -s 100000
-echo " █████▄▄█████  " | lolcat -s 100000
-echo "" | lolcat -s 100000
-echo "-------------------------------" | lolcat
-echo "[Website: www.LearnTermux.tech]" | lolcat -as 50
-echo "-------------------------------" | lolcat 
+echo "▀▀▀▄██▀▀██▄▀▀▀ | www.LearnTermux.tech" | lolcat -s 100000
+echo " ▄███─██─███▄  | Github:khansaad1275" | lolcat -s 100000
+echo " █████▄▄█████  | Insta: skhan_official" | lolcat -s 100000
+echo "-------------------------------------" | lolcat
+echo "---[Website: www.LearnTermux.tech]---" | lolcat -as 50
+echo "-------------------------------------" | lolcat 
 
 i=1
-echo -ne  "Enter Phone-Number of Victim: "
+echo "-------------------------------" | lolcat
+echo -ne  "Enter Phone-Number of Victim --> "
+echo "-------------------------------" | lolcat
 read phonenumber
-echo -ne "Number of calls : "
+echo "-------------------------------" | lolcat
+echo -ne "Number of calls --> "
+echo "-------------------------------" | lolcat
 read num
-echo -ne  "Dealy in sec [30 recommended : ]"
+echo "-------------------------------" | lolcat
+echo -ne  "Dealy in sec [30 recommended] --> "
+echo "-------------------------------" | lolcat
 read sleep
 
 until [ $i -gt $num ]
 
 do
-  echo "Number of Call Sent -->" $i
+  echo " "
+  echo "[+]Number of Call Sent -->" $i
   ((i=i+1))
   termux-telephony-call $phonenumber
   j=30
   until [ $j -lt 0 ]
         do
-        echo -ne "Next Call in -->" $j\\r
+        echo -ne "[-]Next Call in -->" $j\\r
         sleep 1
         ((j=j-1))
         done
